d1a07650a792d522932ed7142144bfe4483fd726
rizkysafdila <rizkysafdila@gmail.com>
2025-10-07 16:29:30 +0700

    chore: update gitignore and vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,11 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
   test: {
+    // coverage: {
+    //   enabled: true,
+    //   provider: 'v8',
+    //   reportsDirectory: '../tests/unit/coverage'
+    // },
     environment: 'nuxt',
     environmentOptions: {
       nuxt: {
